ddc35934deabd2c0b0319b5ff44ec126ab83b45a
Vibhush <vibhush.s.sivakumar@gmail.com>
2023-07-10 21:54:32 -0400

    adding code clash

---
+++ CodeClash0.py
@@ -0,0 +1,15 @@
+class HelloWorld:
+    def helloWorlder(self, Heinput):
+        if input == "Hello World!":
+            return "Hello World!"
+        else:
+            pass
+
+def main():
+    tc1 = HelloWorld()
+    string1= input()
+    ans = tc1.helloWorlder(string1)
+    print(ans)
+
+if __name__ == "__main__":
+    main()
